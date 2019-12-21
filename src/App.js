4b9429f856dec484dd36aad97e0c8f413e4badcb
Petrov K <simpleapplicationgroup@gmail.com>
2019-12-21 23:01:01 +0300

    Добаблен header component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.sass';
 
+import Header from './components/header';
 import HomePage from './pages/homepage';
 import ShopPage from './pages/shop';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ HomePage } />
         <Route path='/shop' component={ ShopPage } />
