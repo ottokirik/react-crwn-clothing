1c8602f5410601f780c1a21beda1e30aa5a8a17b
Petrov K <simpleapplicationgroup@gmail.com>
2019-12-21 22:33:17 +0300

    Добавлены sho page, collection preview component и collection item component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.sass';
 
 import HomePage from './pages/homepage';
+import ShopPage from './pages/shop';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/shop' component={ ShopPage } />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,20 +3,22 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.sass';
 
-const MenuItem = ({ title, imageUrl, size, linkUrl, history, match: { url } }) => (
-    <div className={`${size} menu-item`}
-        onClick={() => history.push(`${url}${linkUrl}`)}
-    >
-        <div
-            style={{ 
-                backgroundImage: `url(${imageUrl})`
-            }} 
-            className="background-image" />
-        <div className="content">
-            <h1 className="title">{ title.toUpperCase() }</h1>
-            <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match: { url } }) => {    
+    return (
+        <div className={`${size} menu-item`}
+            onClick={() => history.push(`${url}${linkUrl}`)}
+        >
+            <div
+                style={{ 
+                    backgroundImage: `url(${imageUrl})`
+                }} 
+                className="background-image" />
+            <div className="content">
+                <h1 className="title">{ title.toUpperCase() }</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default withRouter(MenuItem);
